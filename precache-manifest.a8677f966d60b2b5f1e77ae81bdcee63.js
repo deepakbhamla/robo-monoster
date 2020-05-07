9f25ee5681f8a48c7d7cac92fa2b8ad38f35ef0c
Deepak Bhamla <dbhamla7@gmail.com>
2020-05-07 19:28:08 +0530

    Updates

---
+++ precache-manifest.a8677f966d60b2b5f1e77ae81bdcee63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db87b7d8ab818dd7ef16a1b3986da0f",
+    "url": "/robo-monoster/index.html"
+  },
+  {
+    "revision": "f735d9afe7acfe5496fa",
+    "url": "/robo-monoster/static/css/main.ab9f5232.chunk.css"
+  },
+  {
+    "revision": "d917d29af99285196825",
+    "url": "/robo-monoster/static/js/2.9cd64d92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-monoster/static/js/2.9cd64d92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f735d9afe7acfe5496fa",
+    "url": "/robo-monoster/static/js/main.13fd795d.chunk.js"
+  },
+  {
+    "revision": "e3397ec3cde76396549b",
+    "url": "/robo-monoster/static/js/runtime-main.65eadb08.js"
+  }
+]);
